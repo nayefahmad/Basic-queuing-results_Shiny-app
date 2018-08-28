e9b37098b3acfb2ae11a405a7a965106308d4921
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2018-08-27 22:02:39 -0700

    notes on queuing theory

--- src/app.R
+++ src/app.R
@@ -10,6 +10,7 @@ library("tidyverse")
 
 
 #TODO: ----------------------------------- 
+# > separate tabs for formulas and assumptions
 
 #*****************************************
 
@@ -104,10 +105,34 @@ ui <- fluidPage(
       
       # div( ) creates a section of an HTML doc 
       tags$div(
-            tags$p("First paragraph"), 
-            tags$p("Second paragraph"), 
-            tags$p("Third paragraph")
+            tags$p("Let's say you're building a hospital emergency department, and you need to decide how many beds to include in your design. To do so, you have to consider the demand for these beds, which depends on the average number of patients that arrive every day (the 'arrival rate'), and the average amount of time that they require a bed for treatment (the 'service time')."), 
+            tags$p("To keep things very simple at first, let's say you expect that at most, the average arrival rate is 4 patients per day. Also, you expect that the average service time is about 6 hours (so your 'service rate' is 24/6 = 4 patients per day)."), 
+            tags$p("Given these parameters, how many beds should you include in your design?")
       ),
+      
+      h3("Naive solution"),
+      
+      tags$div(
+            tags$p("Well, this is easy - obviously, all you need is 1 bed, right? Even if 4 people show up, they will each require about 6 hours of service per day, so 1 bed can serve all 4 of them."), 
+            tags$p("All you have to do is solve the equation: Supply for beds = Demand for beds"), 
+            tags$p("In this case: " ), 
+            tags$p("==> 4 patients arriving per day * 0.25 days service time (demand) = x beds * 1 day of capacity (supply)"), 
+            tags$p("==> Therefore x = 1")
+      ), 
+      
+      h3("Actual solution"), 
+      
+      tags$div(
+            tags$p("Here one of the fundamental insights of queuing theory: system performance rapidly deteriorates as the resource utilization rate approaches 100%. If arrivals and service times are non-deterministic, there is always a tradeoff between average time in system, and utilization rate."), 
+            tags$p("Take a look at how long your patients will be spending in your emergency department if you only include 1 bed: as the average number of arrivals approaches 4 per day, the average wait time reaches unacceptably high levels - over 40 hours. "),
+            tags$p("Why is this happening? Because even though on average there are 4 patients a day, there will often be more. And even though on average the service time is 6 hours, there are many cases where it is higher. Finally, it is not true that patients arrive sequentially, with a new arrival ocurring only when the current patient leaves. Therefore, even though the service time may be somewhere around 6 hours, many patients will spend a lot of time waiting in a queue, as all the arrivals ahead of them get service."), 
+            tags$p("In actual applications, it is very important to consider this tradeoff between utilization rate and average time in system."), 
+            tags$p(" "), 
+            tags$p(" "), 
+            tags$p(" ") 
+      ), 
+      
+      
       
       # > customize sidebar: ----------------
       # >> Sidebar layout with input and output definitions ----
@@ -126,17 +151,23 @@ ui <- fluidPage(
                   
                   # Input: Slider for the number of bins 
                   sliderInput(inputId = "servers",
-                              label = "Number of Servers:",
+                              label = "Number of Servers (c):",
                               min = 1,
                               max = 50,
-                              value = 5),
+                              value = 1),
                   
                   # Input: Slider that doesn't really do anything 
                   sliderInput(inputId = "serv.rate",
-                              label = "Service rate (patients/server/day):",
+                              label = "Service rate (mu = patients/server/day):",
                               min = 1,
                               max = 20,
-                              value = 4)
+                              value = 4, 
+                              step = 0.5), 
+                  
+                  # Input: cutoff of y-axis: 
+                  numericInput(inputId = "ymax", 
+                               label = "Cut off y-axis at:", 
+                               value = 50)
                   
             ),
             
@@ -171,7 +202,7 @@ server <- function(input, output) {
       output$distPlot <- renderPlot({
             
             # max visits determined by rho < mu * c 
-            max.visits <- (input$servers * input$serv.rate) - 1  
+            max.visits <- (input$servers * input$serv.rate) - .1  
             
             # create dataframe to plot: 
             df <- data.frame(visits = seq(1, max.visits, length.out = 100),
@@ -188,31 +219,20 @@ server <- function(input, output) {
             # draw plot: 
             df %>% ggplot(aes(x=rho, y=tis)) + 
                   geom_line() + 
-                  # scale_y_continuous(limits = c(0, 24)) +
+                  scale_y_continuous(limits = c(0, input$ymax)) +
                   scale_x_continuous(breaks = seq(0.0, 1.1, 0.1), 
                                      labels = seq(0.0, 1.1, 0.1)) + 
-                  # geom_vline(xintercept = 0.9, 
-                             # colour = "red") + 
-                  labs(title = "Average time in system versus utilization rate (rho)", 
-                       subtitle = paste0("Max number of visits: ", 
+                  geom_hline(yintercept = 24,
+                             colour = "grey90") +
+                  labs(title = "Average time in system versus utilization rate", 
+                       subtitle = paste0("For given mu and c, utilization increases as avg. arrivals increases\nMax arrival rate per day: ", 
                                          max.visits), 
                        x = "Resource utilization", 
-                       y = "Time in system (hours)") + 
-                  theme_classic(base_size = 12)
+                       y = "Average time in system (hours)") + 
+                  theme_classic(base_size = 18)
             
-            
-            
-            # mtcars %>%
-            #       ggplot(aes(x = mpg)) + 
-            #       geom_histogram(bins = input$servers) + 
-            #       labs(title = paste0("Histogram of mtcars$mpg \nTest value: ", 
-            #                           input$serv.rate, # reference to input with ID "test"
-            #                           "\nMax visits (reactive): ",
-            #                           max.visits, 
-            #                           "\nTest imported fn (nonreactive): ",
-            #                           testval.nonreactive))
-            
-      })
+      }, 
+      width = 700)
       
 }
 
